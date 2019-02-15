90594bda722b16763fd94799b30df222f609b0d7
Agustín Romo <39562887+agustinromo@users.noreply.github.com>
2019-02-14 21:41:31 -0600

    Add files via upload
    java codes

---
+++ java/HolaMundo.java
@@ -0,0 +1,12 @@
+import java.lang
+HolaMundo.java 
+public class HolaMundo 
+{  
+       public static void main(String []args){ 
+           System.out.println("Hola Mundo");  
+           Hombre carlos=new Hombre(); 
+            
+          }  
+           carlos.respirar(); 
+      } 
+         
